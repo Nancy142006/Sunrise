c16bbe3492cab8da27f4b4fddaaa5a95ba640daa
Nancy142006 <Nancy14kapoor2006@gmail.com>
2025-05-22 13:41:01 +0530

    imported svg icons

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,6 @@
-import { Bell, User, LogOut, Menu } from "lucide-react";
+import user from "../assets/icons/user.svg";
+import notification from "../assets/icons/notification.svg";
+import { Menu } from "lucide-react";
 import { useState } from "react";
 import SunriseLogo from "../assets/sunrise-logo.svg";
 const Navbar = () => {
@@ -9,33 +11,32 @@ const Navbar = () => {
   return (
     <div className="bg-[#292221] text-white ">
       {/* Top navbar */}
-      <div className="px-4 py-2 flex justify-between items-center">
+      <div className="px-4 py-4 flex justify-between items-center">
         {/* Logo and Title */}
         <div className="flex items-center space-x-8">
           <div className="text-[#FFC235] font-extralight font-montserrat text-[16] space-x-5">
             <img src={SunriseLogo} alt="logo" className="h-10" />
-            <span className="absolute top-4 left-20 tracking-[0.15em]">
+            <span className="absolute top-6 left-20 tracking-[0.15em]">
               COMPASS SCOPE
             </span>
           </div>
         </div>
 
         {/* Icons */}
-        <div className="flex items-center space-x-4">
-          <Bell className="w-5 h-5 text-yellow-400" />
-          <User className="w-5 h-5 text-yellow-400" />
-          <LogOut className="w-5 h-5 text-yellow-400" />
+        <div className="flex items-center space-x-10 ">
+          <img src={notification} alt="icon" className="w-4 h-4" />
+          <img src={user} alt="icon" className="w-6 h-6" />
         </div>
       </div>
 
-      <div className="w-full bg-[#292221] text-white flex items-center px-4 ml-20 border-t-2 border-l-2 border-black">
+      <div className="w-full bg-[#292221] text-white flex items-center px-16 ml-20 border-t-2 border-l-2 border-black">
         {/* Hamburger Menu */}
         <div className="p-2 cursor-pointer">
           <Menu className="w-6 h-6 text-white" />
         </div>
 
         {/* Tabs */}
-        <div className="flex space-x-20 ml-10 relative font-inter ">
+        <div className="flex space-x-20 ml-20 relative font-inter ">
           {tabs.map((tab) => (
             <button
               key={tab}
