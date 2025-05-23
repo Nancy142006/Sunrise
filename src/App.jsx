9210fe37ffe2b838460e641191f60de6dcd5e7f2
Nancy142006 <Nancy14kapoor2006@gmail.com>
2025-05-23 08:01:28 +0530

    created routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
-import { useState } from 'react'
-import './App.css'
-import Login from "./components/Login"
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Login from "./pages/Login";
+import Dashboard from "./pages/Dashboard";
 
 function App() {
   return (
-   <Login/>
-      );
+    <Router>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+      </Routes>
+    </Router>
+  );
 }
 
-
-export default App
+export default App;
