c9506ed72fd380ac1c40870594baf605e1bc33c1
Nancy142006 <Nancy14kapoor2006@gmail.com>
2025-05-24 11:56:05 +0530

    made some basic changes to dashboard

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -5,19 +5,18 @@ import RiskIndicator from "../components/RiskIndicator";
 function Dashboard() {
 
   return (
-    <div className="flex h-screen overflow-hidden">
+    <div className="flex h-screen  overflow-hidden">
       <Sidebar />
       <div className="flex flex-col flex-1">
         <Navbar />
         <main className="flex-1 overflow-auto bg-[#f4efe5]">
           <Mininavbar />
-          <div className="flex gap-[169px] mt-2 ml-[210px]">
+          <div className="flex gap-[169px] mt-2 ml-[210px] ">
             <RiskIndicator />
             <RiskIndicator />
             <RiskIndicator />
           </div>
-          <section className>
-          </section>
+          <section></section>
         </main>
       </div>
     </div>
