ad7236559a8573173a7af3bfa99c7d76e6825e68
Nancy142006 <nancy14kapoor2006@gmail.com>
2025-05-30 15:43:25 +0530

    added a route for signup page

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,13 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Login from "./pages/Login";
 import Dashboard from "./pages/Dashboard";
 import Report from "./pages/Report";
-
+import Signup from "./pages/Signup";
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Login />} />
+        <Route path="/signup" element={<Signup/>}/>
         <Route path="/dashboard" element={<Dashboard />} />
         <Route path="/report" element={<Report/>}/>
       </Routes>
