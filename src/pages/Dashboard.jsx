004f4839f5cb784fb9c91bddf4b354a83cb56134
Nancy142006 <nancy14kapoor2006@gmail.com>
2025-06-02 09:52:57 +0530

    added riskindicator, report card and honeycomb component

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -2,26 +2,64 @@ import Sidebar from "../components/Sidebar";
 import Navbar from "../components/Navbar";
 import Mininavbar from "../components/Mininavbar";
 import RiskIndicator from "../components/RiskIndicator";
-function Dashboard() {
+import Honeycomb from "../components/Honeycomb";
+import ReportCard from "../components/Report";
 
+function Dashboard() {
   return (
-    <div className="flex h-screen  overflow-hidden">
-      <Sidebar />
-      <div className="flex flex-col flex-1">
+    <div className="h-screen text-white overflow-hidden">
+      <div className="fixed top-0 left-0 w-full z-50">
         <Navbar />
-        <main className="flex-1 overflow-auto bg-[#f4efe5]">
-          <Mininavbar />
-          <div className="flex gap-[169px] mt-2 ml-[210px] ">
-            <RiskIndicator />
-            <RiskIndicator />
-            <RiskIndicator />
-          </div>
-          <section></section>
-        </main>
       </div>
+
+      <div className="fixed  top-[68px] left-0 w-full z-40">
+        <Mininavbar />
+      </div>
+
+      <div className="fixed top-[125px] left-5 w-[80px] z-30">
+        <Sidebar />
+      </div>
+
+      <main className="pt-[112px] pl-[100px] h-full overflow-y-auto">
+        <div className="px-12 py-4">
+          <div className="font-montserrat text-white flex items-center justify-between px-4 py-2">
+            <span className="font-bold text-[16px]">Dashboard</span>
+            <button className="bg-[#E6A740] hover:bg-yellow-600 text-black py-2 px-4 rounded">
+              Change Indicators
+            </button>
+          </div>
+          <div className="flex flex-col mt-8 gap-8">
+            <div className="flex gap-14">
+              <RiskIndicator
+                title="Liquidity Ratio"
+                value={30}
+                description="Measures the ability to meet short-term obligations."
+              />
+              <RiskIndicator
+                title="Cash Flow Variability"
+                value={50}
+                description="Flags inconsistency in income and expenses."
+              />
+              <RiskIndicator
+                title="Debt-to-Equity Ratio"
+                value={70}
+                description="Assesses the level of financial leverage."
+              />
+            </div>
+
+            <div className="flex flex-col md:flex-row gap-[30px]">
+              <div className="">
+                <ReportCard />
+              </div>
+              <div>
+                <Honeycomb />
+              </div>
+            </div>
+          </div>
+        </div>
+      </main>
     </div>
   );
 }
 
 export default Dashboard;
-
