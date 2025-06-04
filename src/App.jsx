dae4dcb0259b67001abdc411b322a533a36bba3c
Nancy142006 <nancy14kapoor2006@gmail.com>
2025-06-04 08:46:47 +0530

    created routes for section 1,2 and 3

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Login from "./pages/Login";
 import Dashboard from "./pages/Dashboard";
-// import Report from "./pages/Report";
 import Register from "./pages/Register";
 import Risk from "./pages/Risk";
+import Section1 from "./pages/Section1";
+import Section2 from "./pages/Section2";
+import Section3 from "./pages/Section3";
+
 function App() {
   return (
     <Router>
@@ -12,6 +15,9 @@ function App() {
         <Route path="/register" element={<Register />} />
         <Route path="/dashboard" element={<Dashboard />} />
         <Route path="/risk-indicators" element={<Risk />} />
+        <Route path="/section-1" element={<Section1 />} />
+        <Route path="/section-2" element={<Section2 />} />
+        <Route path="/section-3" element={<Section3 />} />
       </Routes>
     </Router>
   );
